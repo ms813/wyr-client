823484b94131b23cd86f9d57ff6fc0e81505bc1e
= <matthew.smith813@gmail.com>
2019-12-07 13:51:34 +0000

    Style game over component

--- src/host/HostGameOver.js
+++ src/host/HostGameOver.js
@@ -1,6 +1,9 @@
 import React, {useContext, useEffect} from 'react';
 import SpeechContext from '../speech/SpeakTtsContext';
 import {SpeechEvent} from '../speech/SpeechService';
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
+import {Typography} from '@material-ui/core';
 
 const HostGameOver = () => {
     const speech = useContext(SpeechContext);
@@ -10,11 +13,15 @@ const HostGameOver = () => {
     }, [speech]);
 
     return (
-        <div>
-            The game has finished.
-            Thanks for playing!
-            <button onClick={() => window.location.reload()}>Again?</button>
-        </div>
+        <Box py={2} display="flex" flexDirection="column" alignItems="center" justifyContent="space-around" height="75vh">
+            <Typography display="block">
+                The game has finished.
+                Thanks for playing!
+            </Typography>
+            <Box py={2}>
+                <Button variant="contained" color="primary" onClick={() => window.location.reload()}>Again?</Button>
+            </Box>
+        </Box>
     );
 };
 
