47640f1d715d59bea73548edf94aca39011ca5e5
= <matthew.smith813@gmail.com>
2019-12-09 14:28:12 +0000

    Style player game over component

--- src/player/PlayerGameOver.js
+++ src/player/PlayerGameOver.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import {Typography} from '@material-ui/core';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
 
-const PlayerGameOver = () => <div>
-    The game has finished, thanks for playing!
-    <button onClick={() => window.location.reload()}>Again?</button>
-</div>;
+const PlayerGameOver = () => (
+    <Box py={2} display="flex" flexDirection="column" alignItems="center" justifyContent="space-around" height="75vh">
+        <Typography display="block">
+            The game has finished.
+            Thanks for playing!
+        </Typography>
+        <Box py={2}>
+            <Button variant="contained" color="primary" onClick={() => window.location.reload()}>Again?</Button>
+        </Box>
+    </Box>
+);
 
 export default PlayerGameOver;
